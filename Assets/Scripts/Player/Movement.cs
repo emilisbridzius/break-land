a14270a23e5c221769457f141746c553270007c3
dkdumpling <79526410+emilisbridzius@users.noreply.github.com>
2024-07-18 16:29:36 +0100

    crouch and slide fix + animations

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,14 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
-    [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
-    [SerializeField] float jumpHeight, crouchSpeed;
+    [SerializeField, Range(0f, 100f)] float maxAcceleration = 100f;
+    [SerializeField] float jumpHeight, crouchSpeed, slideForce;
     [SerializeField] Transform playerInputSpace, feet, head;
     public FirstPersonCam camController;
     public Collider standingCol, crouchedCol;
@@ -16,12 +14,12 @@ public class Movement : MonoBehaviour
     public float groundRayMaxDist, ceilingRayMaxDist;
     public LayerMask groundLayer, obstructionLayer;
     public bool canJump, canStand;
-    public bool isGrounded, isCrouched, isCamLerping;
+    public bool isGrounded, isCrouched, isCamLerping, isSliding;
 
     Vector3 velocity, desiredVelocity, upAxis;
     Rigidbody body;
     RaycastHit abovePlayer;
-    Coroutine lerpCam;
+    Coroutine lerpCam, slideRoutine;
 
     private void Start()
     {
@@ -52,54 +50,53 @@ public class Movement : MonoBehaviour
             right.Normalize();
 
             desiredVelocity = (forward * playerInput.y + right * playerInput.x) * maxSpeed;
-
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
-            if (canJump)
-            {
-                Jump();
-            }
+            Jump();
         }
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            if (isGrounded)
+            if (isGrounded && body.velocity.magnitude > 0.7f)
+            {
+                if (!isSliding && !isCrouched)
+                {
+                    slideRoutine = StartCoroutine(Slide());
+                }
+                Crouched();
+            }
+            else if (isGrounded)
             {
                 Crouched();
             }
         }
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        else if (Input.GetKeyUp(KeyCode.LeftShift) && isGrounded && canStand)
         {
-            if (isGrounded && canStand)
-            {
-                Standing();
-            }
-            else
-            {
-                Crouched();
-            }
+            Standing();
         }
-
     }
 
     private void FixedUpdate()
     {
-        upAxis = -Physics.gravity.normalized;
+        if (!isSliding)
+        {
+            upAxis = -Physics.gravity.normalized;
 
-        velocity = body.velocity;
-        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+            velocity = body.velocity;
+            float maxSpeedChange = maxAcceleration * Time.deltaTime;
 
-        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+            velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+            velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
 
-        body.velocity = velocity;
+            body.velocity = velocity;
+        }
     }
 
     void Jump()
     {
-        velocity.y += 5;
+        velocity.y += Mathf.Sqrt(2 * jumpHeight * (-Physics.gravity.y * 0.5f));
         body.velocity = velocity;
     }
 
@@ -110,10 +107,17 @@ public class Movement : MonoBehaviour
             StopCoroutine(lerpCam);
         }
 
+        if (slideRoutine != null)
+        {
+            StopCoroutine(slideRoutine);
+            isSliding = false;
+        }
+
         lerpCam = StartCoroutine(LerpCamera(camController.camPos, crouchSpeed));
         maxSpeed = 10f;
 
         isCrouched = false;
+        isSliding = false;
         standingCol.enabled = true;
         crouchedCol.enabled = false;
     }
@@ -131,6 +135,25 @@ public class Movement : MonoBehaviour
         isCrouched = true;
         standingCol.enabled = false;
         crouchedCol.enabled = true;
+    }
+
+    private IEnumerator Slide()
+    {
+        isSliding = true;
+        float slideDuration = 1.5f;
+        float slideTimer = 0f;
+
+        Vector3 slideVelocity = camController.orientation.forward * slideForce;
+        body.velocity = new Vector3(slideVelocity.x, body.velocity.y, slideVelocity.z);
+
+        while (slideTimer < slideDuration)
+        {
+            slideTimer += Time.deltaTime;
+            float slideProgress = slideTimer / slideDuration;
+            body.velocity = Vector3.Lerp(slideVelocity, Vector3.zero, slideProgress);
+            body.AddForce(Vector3.down * 10f);
+            yield return null;
+        }
     }
 
     IEnumerator LerpCamera(Transform targetPos, float duration)
@@ -178,7 +201,5 @@ public class Movement : MonoBehaviour
                 Standing();
             }
         }
-
-        //Debug.DrawRay(head.transform.position, Vector3.up * 1f, Color.red);
     }
 }
